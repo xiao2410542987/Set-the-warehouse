8f33d75a804891977409533ba36b8f3ee83994bd
xiao2410542987 <2410542987@qq.com>
2020-05-04 03:24:08 +0800

    renyu

--- storage/src/test/java/com/renyu/StorageApplicationTests.java
+++ storage/src/test/java/com/renyu/StorageApplicationTests.java
@@ -1,14 +1,19 @@
 package com.renyu;
 
+import com.mysql.cj.MysqlConnection;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StorageApplicationTests {
 
+    @Autowired
+
     @Test
     void contextLoads() {
 	//de
+
     }
 
 }
