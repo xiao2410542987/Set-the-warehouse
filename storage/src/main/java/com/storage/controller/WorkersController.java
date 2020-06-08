3e232005351cd0727325fe6eede98f42d097083a
renyu <2410542987@qq.com>
2020-06-08 15:59:44 +0800

    renyu

--- storage/src/main/java/com/storage/controller/WorkersController.java
+++ storage/src/main/java/com/storage/controller/WorkersController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
