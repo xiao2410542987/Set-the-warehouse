3c175f4d98bbf339818a33ab9545c34664ca34e4
mrzhinice <52566759+mrzhinice@users.noreply.github.com>
2020-06-11 21:58:09 +0800

    del

--- storage/src/main/java/com/storage/controller/WorkersController.java
+++ storage/src/main/java/com/storage/controller/WorkersController.java
@@ -43,10 +43,7 @@ public class WorkersController {
     @Autowired
     private WorkersMapper workersMapper;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 5c8b5e71ff8e756a6e87de8c9b605af095b2bbb4
     @ApiOperation("员工注册")
     @RequestMapping(value = "/register",method = RequestMethod.GET)
 //    @ApiImplicitParams(
