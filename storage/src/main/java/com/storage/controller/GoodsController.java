ba3b522cb3dc4c4ab90c88e80ab852f8c5e02c0b
mrzhinice <52566759+mrzhinice@users.noreply.github.com>
2020-06-13 21:02:24 +0800

    good

--- storage/src/main/java/com/storage/controller/GoodsController.java
+++ storage/src/main/java/com/storage/controller/GoodsController.java
@@ -1,9 +1,13 @@
 package com.storage.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.storage.mapper.GoodsMapper;
+import com.storage.pojo.Goods;
+import com.storage.pojo.Msg;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,7 +19,62 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/storage/goods")
+@CrossOrigin
 public class GoodsController {
 
+    @Autowired
+    private GoodsMapper goodsMapper;
+
+    @ApiOperation("提交新订单")
+    @RequestMapping(value = "/addGoods",method = RequestMethod.POST)
+    public Msg addGoods(@RequestParam @ApiParam(name = "goodsname" ,value = "订单物品名称") String goodsname, @RequestParam @ApiParam(name = "weight" ,value = "重量") int weight,
+                            @RequestParam @ApiParam(name = "meteringid" ,value = "数量单位") int meteringid, @RequestParam @ApiParam(name = "username" ,value = "客户名称") String username,
+                            @RequestParam @ApiParam(name = "userphone" ,value = "客户电话") String userphone, @RequestParam @ApiParam(name = "companyid" ,value = "公司id") int companyid,
+                            @RequestParam @ApiParam(name = "warehouseid" ,value = "仓库位置") int warehouseid, @RequestParam @ApiParam(name = "state" ,value = "订单状态") int state)
+    {
+        Goods goods = new Goods();
+        goods.setGoodsname(goodsname);
+        goods.setWeight(weight);
+        goods.setMeteringid(meteringid);
+        goods.setUsername(username);
+        goods.setUserphone(userphone);
+        goods.setCompanyid(companyid);
+        goods.setWarehouseid(warehouseid);
+        goods.setState(state);
+
+        int i = goodsMapper.insert(goods);
+        if(i==0)
+        {
+            return Msg.fail().add("tips","添加失败");
+        }
+        return Msg.success().add("tips","添加成功");
+    }
+
+    @ApiOperation("提交新订单")
+    @RequestMapping(value = "/updGoods",method = RequestMethod.POST)
+    public Msg updGoods(@RequestParam @ApiParam(name = "id" ,value = "id") int id,
+            @RequestParam @ApiParam(name = "goodsname" ,value = "订单物品名称") String goodsname, @RequestParam @ApiParam(name = "weight" ,value = "重量") int weight,
+                            @RequestParam @ApiParam(name = "meteringid" ,value = "数量单位") int meteringid, @RequestParam @ApiParam(name = "username" ,value = "客户名称") String username,
+                            @RequestParam @ApiParam(name = "userphone" ,value = "客户电话") String userphone, @RequestParam @ApiParam(name = "companyid" ,value = "公司id") int companyid,
+                            @RequestParam @ApiParam(name = "warehouseid" ,value = "仓库位置") int warehouseid, @RequestParam @ApiParam(name = "state" ,value = "订单状态") int state)
+    {
+        Goods goods = new Goods();
+        goods.setId(id);
+        goods.setGoodsname(goodsname);
+        goods.setWeight(weight);
+        goods.setMeteringid(meteringid);
+        goods.setUsername(username);
+        goods.setUserphone(userphone);
+        goods.setCompanyid(companyid);
+        goods.setWarehouseid(warehouseid);
+        goods.setState(state);
+
+        int i = goodsMapper.updateById(goods);
+        if(i==0)
+        {
+            return Msg.fail().add("tips","修改失败");
+        }
+        return Msg.success().add("tips","修失败成功");
+    }
 }
 
--- storage/src/main/java/com/storage/mapper/GoodsMapper.java
+++ storage/src/main/java/com/storage/mapper/GoodsMapper.java
@@ -4,6 +4,8 @@ import com.storage.pojo.Goods;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -14,5 +16,5 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface GoodsMapper extends BaseMapper<Goods> {
-
+        List<Goods> selectAllGoods(int companyid);
 }
