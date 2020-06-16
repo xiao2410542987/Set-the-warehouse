eef063fda6e36167cead4e05b412c171357c4453
mrzhinice <52566759+mrzhinice@users.noreply.github.com>
2020-06-16 15:49:37 +0800

    删除works方法

--- storage/src/main/java/com/storage/controller/WorksController.java
+++ storage/src/main/java/com/storage/controller/WorksController.java
@@ -1,10 +1,12 @@
 package com.storage.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.storage.mapper.WorksMapper;
+import com.storage.pojo.Msg;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -18,6 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/storage/works")
 @CrossOrigin
 public class WorksController {
-    
+    @Autowired
+    private WorksMapper worksMapper;
+
+    @ApiOperation("修改任务")
+    @RequestMapping(value = "/updWork",method = RequestMethod.POST)
+    public Msg updWork(@RequestParam @ApiParam(name = "companyid" ,value = "所属公司外键") int companyid)
+    {
+        return Msg.success();
+    }
+    @ApiOperation("删除任务")
+    @RequestMapping(value = "/delWork",method = RequestMethod.POST)
+    public Msg delWork(@RequestParam @ApiParam(name = "id" ,value = "id") int id)
+    {
+        int i = worksMapper.deleteById(6);
+        if(i==0)
+        {
+            return Msg.fail().add("tips","删除失败");
+        }
+        return Msg.success().add("tips","删除成功");
+    }
+
 }
 
